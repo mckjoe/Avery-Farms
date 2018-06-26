e6deb48bccbf22943da56e2366833fa581937ce3
Joe McKinney and Semhar Kidane <semhar@>
2018-06-26 13:20:27 -0700

    create a folder

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+ function App(){
+   return(<p>hello</p>)
+ }
+ export default App
---
+++ src/components/Body.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Body(){
+  return(
+    <h1>Farmer's Market Circuit</h1>
+  )
+}
+
+export default Body
